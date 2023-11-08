488a59622c94ef602e204e0b8b1236cd45dcf456
Luvianto <lsubrata73@gmail.com>
2023-11-08 23:10:46 +0700

    login_page_part_1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lihat_lukis/pages/login_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,9 +10,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: LoginPage(),
     );
   }
 }
