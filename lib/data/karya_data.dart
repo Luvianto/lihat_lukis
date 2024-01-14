cb6b5f6e53c68bbf3125991a5ad9d2b3417b556b
Bintang Perdana Putra <bintangperdanaputra87@gmail.com>
2024-01-14 21:00:58 +0700

    ADD more data in karya_data.dart with images

--- lib/data/karya_data.dart
+++ lib/data/karya_data.dart
@@ -6,7 +6,7 @@ var karyaList = [
     artist: 'Vincent Van Gogh',
     yearMade: '1889',
     description:
-        'Lukisan ini menunjukkan langit bintang yang berputar di atas desa dengan pegunungan. Van Gogh mengekspresikan emosinya melalui warna dan goresan kuas yang khas, menciptakan atmosfer yang intens dan dramatis.z',
+        'Lukisan ini menunjukkan langit bintang yang berputar di atas desa dengan pegunungan. Van Gogh mengekspresikan emosinya melalui warna dan goresan kuas yang khas, menciptakan atmosfer yang intens dan dramatis.',
     imageAsset: 'lib/images/la-nuit-etoilee.jpg',
   ),
   Karya(
@@ -63,5 +63,109 @@ var karyaList = [
     description:
         "Lukisan ini menampilkan sosok yang menjerit di atas jembatan dengan latar belakang langit merah menyala. Munch menciptakan karya ini sebagai ekspresi ketidakpastian dan kecemasan yang dirasakannya.",
     imageAsset: 'lib/images/The-Scream.jpg',
+  ),
+  Karya(
+    title: 'The Birth of Venus',
+    artist: 'Sandro Botticelli',
+    yearMade: '1484-1486',
+    description:
+        "Lukisan ini menggambarkan dewi Venus yang muncul dari laut di atas sebuah kerang. Karya ini menjadi simbol keindahan dan keanggunan dalam seni Renaissance Italia.",
+    imageAsset: 'lib/images/The-Birth-of-Venus.jpg',
+  ),
+  Karya(
+    title: 'The Night Watch',
+    artist: 'Rembrandt van Rijn',
+    yearMade: '1642',
+    description:
+        "Lukisan ini menggambarkan kelompok tentara yang bersiap untuk berangkat, menciptakan suasana dramatis dengan penggunaan cahaya dan bayangan yang khas Rembrandt.",
+    imageAsset: 'lib/images/The-Night-Watch.jpg',
+  ),
+  Karya(
+    title: 'American Gothic',
+    artist: 'Grant Wood',
+    yearMade: '1930',
+    description:
+        "Lukisan ini menampilkan seorang petani dan istrinya berdiri di depan rumah dengan latar belakang gudang lumbung. Karya ini sering diinterpretasikan sebagai representasi kehidupan pedesaan Amerika pada era Depresi.",
+    imageAsset: 'lib/images/American-Gothic.jpg',
+  ),
+  Karya(
+    title: "Les Demoiselles d'Avignon",
+    artist: 'Pablo Picasso',
+    yearMade: '1907',
+    description:
+        "Lukisan ini menggambarkan lima wanita telanjang dengan wajah yang terdistorsi dan bentuk tubuh yang geometris. Karya ini dianggap sebagai salah satu karya awal dalam periode kubisme.",
+    imageAsset: 'lib/images/Les-Demoiselles-dAvignon.jpg',
+  ),
+  Karya(
+    title: "Whistler's Mother",
+    artist: 'James McNeill Whistler',
+    yearMade: '1871',
+    description:
+        "Potret ini menampilkan ibu seniman, Anna McNeill Whistler, yang duduk dengan sikap tenang. Karya ini dikenal karena kesederhanaannya dan penggunaan warna netral.",
+    imageAsset: 'lib/images/Whistlers-Mother.jpg',
+  ),
+  Karya(
+    title: 'The Great Wave off Kanagawa',
+    artist: 'Katsushika Hokusai',
+    yearMade: '1830-1833',
+    description:
+        "Lukisan ini adalah bagian dari serangkaian karya yang menggambarkan pemandangan Gunung Fuji dengan ombak besar di depannya. Karya ini merupakan salah satu contoh paling terkenal dari seni ukiyo-e Jepang.",
+    imageAsset: 'lib/images/The-Great-Wave-off-Kanagawa.jpg',
+  ),
+  Karya(
+    title: 'Nighthawks',
+    artist: 'Edward Hopper',
+    yearMade: '1942',
+    description:
+        "Lukisan ini menunjukkan sebuah diner pada malam hari dengan tiga orang pelanggan dan seorang pelayan. Hopper menciptakan atmosfer yang khas dengan pencahayaan rendah dan suasana yang hening.",
+    imageAsset: 'lib/images/Nighthawks.jpg',
+  ),
+  Karya(
+    title: 'The Raft of the Medusa',
+    artist: 'Théodore Géricault',
+    yearMade: '1818-1819',
+    description:
+        "Lukisan ini menggambarkan peristiwa kapal Medusa yang karam, menunjukkan para penyintas yang terdampar di rakit improvisasi. Karya ini menghadirkan dramatisme dan perasaan keputusasaan.",
+    imageAsset: 'lib/images/The-Raft-of-the-Medusa.jpg',
+  ),
+  Karya(
+    title: 'The Garden of Earthly Delights',
+    artist: 'Hieronymus Bosch',
+    yearMade: '1490-1510',
+    description:
+        "Lukisan triptych ini menunjukkan kehidupan manusia dari surga, dunia, hingga neraka. Bosch menciptakan dunia yang penuh dengan makhluk aneh dan simbolisme misterius.",
+    imageAsset: 'lib/images/The-Garden-of-Earthly-Delights.jpg',
+  ),
+  Karya(
+    title: 'The Kiss',
+    artist: 'Gustav Klimt',
+    yearMade: '1907-1908',
+    description:
+        "Karya ini menampilkan sepasang kekasih yang berada dalam pelukan mesra, dihiasi dengan motif emas dan hiasan geometris khas Klimt. Lukisan ini merupakan representasi romantisme dan simbolisme pada awal abad ke-20.",
+    imageAsset: 'lib/images/The-Kiss.jpg',
+  ),
+  Karya(
+    title: 'The Luncheon of the Boating Party',
+    artist: 'Pierre-Auguste Renoir',
+    yearMade: '1880-1881',
+    description:
+        "Lukisan ini menggambarkan sekelompok orang yang menikmati makan siang di tepi sungai Seine. Renoir menangkap momen yang penuh kebahagiaan dan keceriaan dengan warna-warna cerah.",
+    imageAsset: 'lib/images/The-Luncheon-of-the-Boating-Party.jpg',
+  ),
+  Karya(
+    title: 'The School of Athensy',
+    artist: 'Raphael',
+    yearMade: '1509-1511',
+    description:
+        "Bagian dari dekorasi untuk Vatican Palace, karya ini menampilkan para filsuf besar dan tokoh-tokoh berpengaruh dari sejarah, menciptakan gambaran klasik tentang kebijaksanaan dan pengetahuan.",
+    imageAsset: 'lib/images/The-School-of-Athens.jpg',
+  ),
+  Karya(
+    title: 'The Death of Sardanapalus',
+    artist: 'Eugène Delacroix',
+    yearMade: '1827',
+    description:
+        "Lukisan ini menggambarkan kehancuran kerajaan Sardanapalus, Raja Asyur, yang memilih untuk bunuh diri bersama dengan harta bendanya daripada menyerah kepada musuh. Delacroix mengekspresikan dramatisisme dan kekuatan melalui warna dan gerakan.",
+    imageAsset: 'lib/images/The-Death-of-Sardanapalus.jpg',
   ),
 ];
